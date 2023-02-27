151fd71872186da0a0a6f08a09b57c31c237e896
savinnsk <savio.document@gmail.com>
2023-02-27 20:21:21 -0300

    feat: add button element

--- classes/class01/pages/video-player/src/video-factory.js
+++ classes/class01/pages/video-player/src/video-factory.js
@@ -1,8 +1,28 @@
 import Camera from "../../../lib/shared/camera.js";
+import { supportWorkerType } from "../../../lib/shared/utils.js";
 
 import VideoController from "./video-controller.js";
 import VideoService from "./video-service.js";
 import VideoView from "./video-view.js";
+
+async function getWorker() {
+  if (supportWorkerType()) {
+    console.log("supports");
+    //using src due to, it'll be injected in index reference
+    const worker = new Worker("./src/video-worker.js");
+    return;
+  }
+
+  const workerMock = {
+    async postMessage() {},
+    onmessage(msg) {},
+  };
+
+  console.log("not supports");
+  return workerMock;
+}
+
+const worker = await getWorker();
 
 const camera = await Camera.init();
 
