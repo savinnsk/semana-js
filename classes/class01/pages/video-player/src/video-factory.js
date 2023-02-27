adea30d3e0f842663e170d4bf8d2a7612e5f84fa
savinnsk <savio.document@gmail.com>
2023-02-27 19:42:42 -0300

    feat: add camera component

--- classes/class01/pages/video-player/src/video-factory.js
+++ classes/class01/pages/video-player/src/video-factory.js
@@ -1,14 +1,18 @@
+import Camera from "../../../lib/shared/camera.js";
+
 import VideoController from "./video-controller.js";
+import VideoService from "./video-service.js";
+import VideoView from "./video-view.js";
+
+const camera = await Camera.init();
 
 const [rootPath] = window.location.href.split("/pages/");
+
 const VideoFactory = {
   async initialize() {
     return VideoController.initialize({
-      //   view: new CardsView(),
-      //   service: new CardsService({
-      //     dbUrl: `${rootPath}/assets/database.json`,
-      //     cardListWorker,
-      //   }),
+      view: new VideoView({}),
+      service: new VideoService({}),
     });
   },
 };
