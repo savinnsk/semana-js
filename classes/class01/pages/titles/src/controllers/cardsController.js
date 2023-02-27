15edb75b2704178b8f497893916e1bafcc3d5314
savinnsk <savio.document@gmail.com>
2023-02-27 18:02:58 -0300

    feat: add card Worker

--- classes/class01/pages/titles/src/controllers/cardsController.js
+++ classes/class01/pages/titles/src/controllers/cardsController.js
@@ -1,53 +1,43 @@
 export default class CardsController {
-  #itemsPerLine = 5
-  #view
-  #service
+  #itemsPerLine = 5;
+  #view;
+  #service;
   constructor({ view, service }) {
-    this.#view = view
-    this.#service = service
+    this.#view = view;
+    this.#service = service;
   }
 
   async #loadDB() {
-    return this.#service.loadCards()
+    return this.#service.loadCards();
   }
 
   addCards(keyword) {
-    const cards = this.#service.filterTitles(keyword)
-    const totalCards = cards.length
+    const cards = this.#service.filterTitles(keyword);
+    const totalCards = cards.length;
 
     if (!totalCards) {
-      this.#view.updateSearchTitleBarTotal(totalCards)
-      return
+      this.#view.updateSearchTitleBarTotal(totalCards);
+      return;
     }
 
-    this.#view.addCards(cards, this.#itemsPerLine)
+    this.#view.addCards(cards, this.#itemsPerLine);
   }
 
   #onSearchInput(keyword) {
-    this.#view.clearCards()
+    this.#view.clearCards();
 
-    console.log('activating blocking operation...')
-    console.time('blocking-op')
-    // blocking function
-    for (let counter = 0; counter < 1e5; counter++) console.log()
-    console.timeEnd('blocking-op')
-
-    console.log('blocking operation freed up ...')
-
-    this.addCards(keyword)
+    this.addCards(keyword);
   }
 
   async init() {
-    await this.#loadDB()
-    this.#view.configureOnSearchInput(
-      this.#onSearchInput.bind(this)
-    )
+    await this.#loadDB();
+    this.#view.configureOnSearchInput(this.#onSearchInput.bind(this));
 
-    this.addCards("")
+    this.addCards("");
   }
 
   static async initialize(deps) {
-    const controller = new CardsController(deps)
-    return controller.init()
+    const controller = new CardsController(deps);
+    return controller.init();
   }
 }
