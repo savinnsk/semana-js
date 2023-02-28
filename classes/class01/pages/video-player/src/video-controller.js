7f51be9ed26447126c747a1e30fd89a8fc2d9612
savinnsk <savio.document@gmail.com>
2023-02-27 22:23:14 -0300

    feat: add button class component

--- classes/class01/pages/video-player/src/video-controller.js
+++ classes/class01/pages/video-player/src/video-controller.js
@@ -1,12 +1,29 @@
 export default class VideoController {
-  constructor({}) {}
+  #videoView;
+  #videoService;
+  constructor({ view, service }) {
+    this.#videoView = view;
+    this.#videoService = service;
+
+    //bind to referer a controller
+    this.#videoView.configureOnBtnClick(this.onBtnStart.bind(this));
+  }
 
   static async initialize(deps) {
     const controller = new VideoController(deps);
+    controller.log("not yer detecting eye blink! click in button to start");
     return controller.init();
   }
 
   async init() {
     console.log("init");
+  }
+
+  log(text) {
+    this.#videoView.log(`logger ${text}`);
+  }
+
+  onBtnStart() {
+    this.log("initializing detection");
   }
 }
--- classes/class01/pages/video-player/src/video-view.js
+++ classes/class01/pages/video-player/src/video-view.js
@@ -1,3 +1,18 @@
 export default class VideoView {
+  #btnInit = document.querySelector("#init");
+  #statusElement = document.querySelector("#status");
+
   constructor({}) {}
+
+  enableButton() {
+    this.#btnInit.disable = false;
+  }
+
+  configureOnBtnClick(fn) {
+    this.#btnInit.addEventListener("click", fn);
+  }
+
+  log(text) {
+    this.#statusElement.innerHTML = text;
+  }
 }
